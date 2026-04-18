c5b795e82c4493e485b81d7981887c6c129b8550
charafi <progpt.co@gmail.com>
2026-04-18 14:34:21 +0200

    refacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-function Button({ text, onClick, disabled }) {
+function Button({ label, onClick, disabled }) {
   return (
     <button onClick={onClick} disabled={disabled}>
-      {text}
+      {label}
     </button>
   );
 }
@@ -13,40 +13,41 @@ function Checkbox({ onChange }) {
   return <input type="checkbox" onChange={(e) => onChange(e.target.checked)} />;
 }
 
+function generateUser() {
+  return Array.from({ length: 4 }, (_, i) => ({
+    id: i + 1,
+    nom: ["toot", "mimi", "nino", "mimi", "nono"][i],
+    score: 0,
+  }));
+}
+
 function App() {
   const [compte, setCompte] = useState(0);
   const [checked, setChecked] = useState(false);
   const [message, setMessage] = useState("");
-  const [list, setList] = useState(
-    Array.from({ length: 4 }, (_, i) => ({
-      id: i + 1,
-      nom: ["toot", "mimi", "nino", "mimi", "nono"][i],
-      score: 0,
-    })),
-  );
+  const [list, setList] = useState(generateUser());
 
-  const incrementer = () => {
+  const onIncrement = () => {
     setCompte(compte + 1);
   };
 
-  const allzero = () => {
+  const onInitialized = () => {
     setList(list.map((j) => ({ ...j, score: 0 })));
   };
 
-  const random = () => {
-    setList(
-      list.map((j) => ({
-        ...j,
-        score: j.score + Math.floor(Math.random() * 33),
-      })),
-    );
+  const onRandom = () => {
+    const newUsers = list.map((j) => ({
+      ...j,
+      score: j.score + Math.floor(Math.random() * 33),
+    }));
+    setList(newUsers);
   };
 
   const sup = (id) => {
     setList(list.filter((j) => j.id !== id));
   };
 
-  const ordoner = () => {
+  const onOrdoner = () => {
     const copie = [...list];
     copie.sort((a, b) => a.score - b.score);
     setList(copie);
@@ -59,23 +60,23 @@ function App() {
   }, [list]);
 
   return (
-    <>
-      <>
+    <div>
+      <div>
         {list.map((j) => (
           <p key={j.id}>
             {j.nom} {j.score}
             <button onClick={() => sup(j.id)}>sup</button>
           </p>
         ))}
-      </>
+      </div>
       <p>{compte}</p>
-      <Button text="valider" disabled={!checked} onClick={incrementer} />
-      <Button text="annuler" onClick={allzero} />
-      <Button text="random" onClick={random} />
-      <Button text="croissant" onClick={ordoner} />
+      <Button label="valider" disabled={!checked} onClick={onIncrement} />
+      <Button label="annuler" onClick={onInitialized} />
+      <Button label="random" onClick={onRandom} />
+      <Button label="croissant" onClick={onOrdoner} />
       {message && <p>{message}</p>}
       <Checkbox onChange={setChecked} />
-    </>
+    </div>
   );
 }
 
