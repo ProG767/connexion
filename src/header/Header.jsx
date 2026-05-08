ac80218d98e90b1e13c0c97901d83984631e3a10
Malek BEN ANES <benanesmalek@gmail.com>
2026-05-08 22:16:35 +0200

    finaliser header avec profile

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,4 +1,13 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { getUserById } from "../api/server";
+
 const Header = () => {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const currentUser = getUserById(id);
+
   return (
     <header className="p-3 mb-3 border-bottom">
       <div className="container">
@@ -26,66 +35,63 @@ const Header = () => {
             ></svg>
           </a>
 
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li>
-              <a href="#" className="nav-link px-2 link-secondary">
-                Overview
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 link-dark">
-                Inventory
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 link-dark">
-                Customers
-              </a>
-            </li>
-          </ul>
-
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input
-              type="search"
-              className="form-control"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-
-          <div className="dropdown text-end">
-            <a
-              className="d-block link-dark text-decoration-none dropdown-toggle"
-              id="dropdownUser1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="32"
-                height="32"
-                className="rounded-circle"
-              />
-            </a>
-            <ul
-              className="dropdown-menu text-small"
-              aria-labelledby="dropdownUser1"
-            >
+          {currentUser && (
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
-                <a className="dropdown-item">Settings</a>
+                <a href="#" className="nav-link px-2 link-secondary">
+                  Accueil
+                </a>
               </li>
               <li>
-                <a className="dropdown-item">Profile</a>
+                <a href="#" className="nav-link px-2 link-dark">
+                  Amis
+                </a>
               </li>
               <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item">Sign out</a>
+                <a href="#" className="nav-link px-2 link-dark">
+                  Posts
+                </a>
               </li>
             </ul>
-          </div>
+          )}
+
+          {currentUser && (
+            <div className="dropdown text-end">
+              <a
+                className="d-block link-dark text-decoration-none dropdown-toggle"
+                id="dropdownUser1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src={currentUser.profilePhoto}
+                  alt="mdo"
+                  width="32"
+                  height="32"
+                  className="rounded-circle"
+                />
+              </a>
+              <ul
+                className="dropdown-menu text-small"
+                aria-labelledby="dropdownUser1"
+              >
+                <li>
+                  <a className="dropdown-item">Settings</a>
+                </li>
+                <li>
+                  <a className="dropdown-item">Profile</a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" onClick={() => navigate("/")}>
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </header>
